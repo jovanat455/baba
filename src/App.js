295a84bf2511e06d2ce2a940df2e8eefbb3388f1
Jovana Todorovic <jotodoro@microsoft.com>
2022-12-26 12:01:58 +0100

    adding web config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Baba test
         </p>
         <a
           className="App-link"
