f17662421fc537f141a7d0c7424e5e9ea3578305
Jovana Todorovic <jotodoro@microsoft.com>
2022-12-28 13:50:37 +0100

    Adding navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import ReceiptForm from './ReceiptForm';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Login from './Pages/ScanReceipt';
+import Profile from './Pages/AddOrder';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ReceiptForm />
+<Router>
+<Navbar />
+  <Routes>
+    <Route excat path="/scanReceipt" element={<Login />}> </Route>
+    <Route excat path="/addOrder" element={<Profile />}> </Route>
+    </Routes>
+  
+</Router>
+
+        {/* <ReceiptForm /> */}
       </header>
     </div>
   );
---
+++ src/Pages/ScanReceipt.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReceiptForm from '../ReceiptForm';
+
+function ScanReceipt() {
+    return(
+              <ReceiptForm /> 
+    );
+}
+
+export  default ScanReceipt;
