8f622bdc6447db16fb549638744365af4ca9f3f5
Vladimir Pavlovic <vpavlovic@microsoft.com>
2022-12-29 14:00:43 +0100

    fixed bogdan's bs

--- src/Pages/StartOrder.js
+++ src/Pages/StartOrder.js
@@ -1,21 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './StartOrder.css';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 function StartOrder() {
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  }
 
-    return (
-        <div>
-            <img src={`${process.env.PUBLIC_URL}/assets/images/startEngines.gif`} className="oopsImage" alt="oops..." />
-            <form onSubmit={handleSubmit}>
-                <Form.Control placeholder="Your name here" className="addOrderTextField" />
-                <button className="rgbButton">Ready...Set...GO!</button>
-            </form>
-        </div>
+  return (
+    <Container className='StartOrder'>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/startEngines.gif`} className="oopsImage" alt="oops..." />
+      <Form onSubmit={handleSubmit} className='StartOrderForm'>
+        <Form.Control placeholder="Your name here" className="addOrderTextField" />
+        <button className="rgbButton">Ready...Set...GO!</button>
+      </Form>
+    </Container>
   );
 }
 
