dae8b7e268646833a4266a7885f6237852ccd342
Vladimir Pavlovic <vpavlovic@microsoft.com>
2022-12-29 14:37:30 +0100

    changed color and logo

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,3 +1,4 @@
+import Alert from 'react-bootstrap/Alert';
 import React from "react";
 import Container from 'react-bootstrap/Container';
 import Image from "react-bootstrap/Image";
@@ -6,7 +7,10 @@ function NotFound() {
   return (
     <Container className='StartOrder'>
       <Image src={`${process.env.PUBLIC_URL}/assets/images/404.webp`} alt="Page not found" fluid />
-
+      <Alert variant="danger">
+        <h1>404!</h1>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+      </Alert>
     </Container>
   );
 }
--- src/components/NavbarLayout.js
+++ src/components/NavbarLayout.js
@@ -2,13 +2,17 @@ import { Outlet, Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Image from "react-bootstrap/Image";
 
 const NavBarLayout = () => {
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand="lg" sticky='top'>
+      <Navbar bg='light' variant='light' expand="lg" sticky='top'>
         <Container>
-          <Navbar.Brand href="/">Baba Delivery</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Image src={`${process.env.PUBLIC_URL}/favicon.png`} alt="Page not found" fluid />{' '}
+            Baba Delivery
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
