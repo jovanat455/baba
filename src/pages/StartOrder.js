47b0a9f05a6fbdfb37a0f8311e0eb87efeed5503
Jovana Todorovic <jotodoro@microsoft.com>
2022-12-30 11:01:17 +0100

    Creating order logic - ou yeah

--- src/pages/StartOrder.js
+++ src/pages/StartOrder.js
@@ -3,10 +3,17 @@ import './StartOrder.css';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Image from "react-bootstrap/Image";
+import axios from 'axios';
+
+const startOrderApi = "https://babadb20221229134825.azurewebsites.net/api/StartOrder?code=Gqo_y9PQPrMz2Tf-5AdgUwELFoOAS_zKbfRkT-UXKDhaAzFuKPuBEA==";
 
 function StartOrder() {
   const handleSubmit = async (event) => {
     event.preventDefault();
+    let apiBuilder = startOrderApi + "&owner=" + event.target[0].value;
+    let result = await axios.get(apiBuilder);
+    //TODO: Add Message that order is created 
+    event.target[0].value = result.data;
   }
 
   return (
